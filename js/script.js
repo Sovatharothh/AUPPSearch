2a78ef2c708ab4e9e7e21d37855fa8305d61cc2d
MOKMonyratanak <2023026mok@aupp.edu.kh>
2024-12-30 15:26:52 +0700

    new file:   assets/images/people.png 	new file:   assets/images/search.png 	modified:   css/styles.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.getElementById("sidebar");
     const toggleSidebar = document.getElementById("toggleSidebar");
+
+    // Toggle sidebar collapsed state when the hamburger icon is clicked
+    toggleSidebar.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
+    });
+
     const menuItems = document.querySelectorAll(".menu-item");
     const contentFrame = document.getElementById("contentFrame");
-
-    // Toggle sidebar visibility
-    toggleSidebar.addEventListener("click", () => {
-        sidebar.classList.toggle("hidden");
-    });
 
     // Load content when menu items are clicked
     menuItems.forEach((item) => {
@@ -15,5 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
             const targetPage = item.getAttribute("data-target");
             contentFrame.src = targetPage;
         });
+    });
+
+    const dropdownArrow = document.querySelector(".dropdown-arrow");
+    const profileContainer = document.querySelector(".profile-container");
+
+    // Toggle dropdown menu visibility
+    dropdownArrow.addEventListener("click", () => {
+        profileContainer.classList.toggle("active");
     });
 });
